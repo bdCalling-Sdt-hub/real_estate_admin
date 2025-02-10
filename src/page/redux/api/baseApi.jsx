1b21ef622cb73d952d0f045e932d39c50b462b53
unknown <foisalrk2@gmail.com>
2025-02-10 18:03:09 +0600

    asdfasdfasdfasdf

---
+++ src/page/redux/api/baseApi.jsx
@@ -0,0 +1,34 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const baseQuery = fetchBaseQuery({
+  baseUrl: "https://backend.volunhelp.com/api/v1",
+  // prepareHeaders: (headers) => {
+  //   const token = localStorage.getItem("accessToken");
+  //   console.log(token);
+  //   if (token) {
+  //     headers.set("Authorization", `${token}`);
+  //   }
+  //   // if (refreshToken) {
+  //   //   headers.set("Authorization", `Bearer ${refreshToken}`);
+  //   // }
+  //   return headers;
+  // },
+  
+  prepareHeaders: (headers, { getState }) => {
+    const token = getState().logInUser.token;
+    console.log("from baseApi", token);
+    if (token) {
+      headers.set("authorization", `${token}`);
+    }
+    return headers;
+  },
+});
+
+export const baseApi = createApi({
+  reducerPath: "baseApi",
+  baseQuery: baseQuery,
+  tagTypes: ["overview", "host"],
+  endpoints: () => ({}),
+});
+
+export const imageUrl = "'https://backend.volunhelp.com";
