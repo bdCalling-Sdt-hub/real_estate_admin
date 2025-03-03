b4290819203be9cd80a49a0a0afd3a4ed8f57a5e
safin-calling <safin248@outlook.com>
2025-03-03 12:18:33 +0600

    Refactor Order Management components for improved structure and data handling

--- src/page/orderManagement/AllManage.jsx
+++ src/page/orderManagement/AllManage.jsx
@@ -12,24 +12,22 @@ export const AllManage = ({ search }) => {
   });
 
   return (
-    <div className="">
-      <Table
-        dataSource={orders?.data?.data}
-        columns={columns}
-        pagination={{
-          current: page,
-          pageSize: pageSize,
-          total: orders?.data?.meta?.total,
-          onChange: (newPage) => {
-            setPage(newPage);
-          },
-          showSizeChanger: false,
-          showTotal: (total) => `Total ${total} items`,
-        }}
-        loading={isLoading}
-        bordered
-        style={{ marginTop: "20px" }}
-      />
-    </div>
+    <Table
+      dataSource={orders?.data?.data}
+      columns={columns}
+      pagination={{
+        current: page,
+        pageSize: pageSize,
+        total: orders?.data?.meta?.total,
+        onChange: (newPage) => {
+          setPage(newPage);
+        },
+        showSizeChanger: false,
+        showTotal: (total) => `Total ${total} items`,
+      }}
+      loading={isLoading}
+      bordered
+      style={{ marginTop: "20px" }}
+    />
   );
 };
